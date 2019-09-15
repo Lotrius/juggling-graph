1f81b2432a7e2da76b7085a52ee0a18d2f7afb57
Somesk278 <skim7420@gmail.com>
2019-09-15 13:40:38 -0400

    Updates

---
+++ precache-manifest.0505ffc8af4ddf1e0b50acd31ddc945b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aca6ccca4be956fd41d0f136c5a320e0",
+    "url": "/juggling-graph/index.html"
+  },
+  {
+    "revision": "3336f5bc1e8236b35914",
+    "url": "/juggling-graph/static/css/0.ff9ea1f9.chunk.css"
+  },
+  {
+    "revision": "a0feb32f4038e2332350",
+    "url": "/juggling-graph/static/css/3.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "2a37afc27ae23b21f8fe",
+    "url": "/juggling-graph/static/css/main.e92517c3.chunk.css"
+  },
+  {
+    "revision": "3336f5bc1e8236b35914",
+    "url": "/juggling-graph/static/js/0.9dd8b9e5.chunk.js"
+  },
+  {
+    "revision": "a0feb32f4038e2332350",
+    "url": "/juggling-graph/static/js/3.9899f049.chunk.js"
+  },
+  {
+    "revision": "16ee5d5bf7db2da2f94e",
+    "url": "/juggling-graph/static/js/4.6ab891aa.chunk.js"
+  },
+  {
+    "revision": "4dcb4e2d33a50d6ae8d8",
+    "url": "/juggling-graph/static/js/5.7860330f.chunk.js"
+  },
+  {
+    "revision": "4f31cc37c5669b96d5aa",
+    "url": "/juggling-graph/static/js/6.2198babe.chunk.js"
+  },
+  {
+    "revision": "f79a198f2a6912a7611c",
+    "url": "/juggling-graph/static/js/7.e95daea6.chunk.js"
+  },
+  {
+    "revision": "2b312e88c386981c2493",
+    "url": "/juggling-graph/static/js/8.c49177da.chunk.js"
+  },
+  {
+    "revision": "8dc057b552e133427502",
+    "url": "/juggling-graph/static/js/9.76875a02.chunk.js"
+  },
+  {
+    "revision": "2a37afc27ae23b21f8fe",
+    "url": "/juggling-graph/static/js/main.ed9b6716.chunk.js"
+  },
+  {
+    "revision": "65b3c92d23f299df6de5",
+    "url": "/juggling-graph/static/js/runtime~main.071b5235.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/juggling-graph/precache-manifest.69e4e14fab934fa1fce7919b5ac2c2d0.js"
+  "/juggling-graph/precache-manifest.0505ffc8af4ddf1e0b50acd31ddc945b.js"
 );
 
 self.addEventListener('message', (event) => {
